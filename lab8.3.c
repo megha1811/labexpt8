15d30d7d4d0c9bb461548c8e5d7931d52e1122d8
Megha Rajesh <105777924+megha1811@users.noreply.github.com>
2022-07-27 15:27:18 +0530

    Add files via upload

---
+++ lab8.3.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include <time.h>
+int array[1000000];
+
+float timedifference_msec(struct timeval t0, struct timeval t1)
+{
+    return (t1.tv_sec - t0.tv_sec) * 1000.0f + (t1.tv_usec - t0.tv_usec) / 1000.0f;
+}
+
+void printArr(int a[], int n){  
+	int i;  
+	for (i = 0; i < n; i++){
+		printf("%d ", a[i]);  
+	}  
+}
+
+int main()
+{
+	printf("Bubble Sort\n");
+	struct timeval t0;
+	struct timeval t1;
+	float elapsed;
+    
+	int i,key,ch;
+	long size,limit;
+	printf("Enter number of Elements:");
+	scanf("%ld",&size);
+	int array[size];
+	
+	int num=size;
+	int j,x,y,temp;
+	srand( (unsigned) time(NULL) * getpid());
+	if(array != NULL)
+	{
+		for(j = 0; j < num; j++)
+		{
+			array[j] = rand()%100;
+		}
+	}
+	printf("Array before sorting:\n");
+	printArr(array,num);				
+	gettimeofday(&t0, NULL);
+	for(x = 0; x < num - 1; x++){       
+		for(y = 0; y < num - x - 1; y++){          
+			if(array[y] > array[y + 1]){               
+				temp = array[y];
+				array[y] = array[y + 1];
+				array[y + 1] = temp;
+			}
+		}
+	}
+	gettimeofday(&t1, NULL);
+	elapsed = timedifference_msec(t0, t1);
+	printf("\nArray after sorting:\n");
+	printArr(array,num);
+	printf("\n\nBubble Sort Code executed in %f milliseconds.\n", elapsed);
+}
